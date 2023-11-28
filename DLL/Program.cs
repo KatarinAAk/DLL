fe4141404aee02abd50144c44c55e32b4c724568
Рэм <iiibragimov05.02@gmail.com>
2023-11-28 08:56:45 +0500

    Добавьте файлы проекта.

---
+++ DLL/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = MyFunctions.Add(5, 3);
+            Console.WriteLine("Результат сложения: " + result);
+
+            int fact = MyFunctions.Factorial(5);
+            Console.WriteLine("Факториал числа 5: " + fact);
+
+            MyFunctions.ReadFile("input.txt");
+
+            MyFunctions.WriteFile("output.txt", "Привет, мир!");
+
+            bool isPrime = MyFunctions.IsPrime(7);
+            Console.WriteLine("Число 7 простое: " + isPrime);
+        }
+    }
+}
---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyLibrary
+{
+    public class MyFunctions
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        public static void ReadFile(string filename)
+        {
+            try
+            {
+                string content = File.ReadAllText(filename);
+                Console.WriteLine(content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading file: " + e.Message);
+            }
+        }
+
+        public static void WriteFile(string filename, string data)
+        {
+            try
+            {
+                File.WriteAllText(filename, data);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error writing to file: " + e.Message);
+            }
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
